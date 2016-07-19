a4743c11c91d2ef5b5181af11c8885ace248dd5d
Nadav Mermer <mermer@bigpanda.io>
2016-07-19 15:15:57 +0300

    saving a status map for all profiles

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const profiler = require('v8-profiler');
 var _datadir;
+const profileStatuses = {};
 
 module.exports.init = (params) => {
   params.app.post('/profile', startProfiling);
   params.app.post('/profile/:timeout', startProfiling);
+  params.app.get('/profile/status/:id', getStatus);
   _datadir = params.datadir;
 };
 
@@ -15,13 +17,34 @@ function startProfiling(req, res) {
   const timeout = req.params && req.params.timeout || 5000;
 
   profiler.startProfiling(id);
-
+  setStatus(id, `Profile started at ${timestamp / 1000} for ${timeout / 1000} seconds`);
   setTimeout(() => stopProfiling(id), timeout);
 
-  res.status(200).send(`Started profiling for ${timeout}s`);
+  res.status(200).send(`Started profiling for ${timeout} seconds\nid: ${id}`);
 }
 
 function stopProfiling(id) {
   const profile = profiler.stopProfiling(id);
-  fs.writeFile(`${_datadir}/${id}.cpuprofile`, JSON.stringify(profile));
+  try {
+    fs.writeFile(`${_datadir}/${id}.cpuprofile`, JSON.stringify(profile));
+    setStatus(id, `Profile finished successfuly on ${Date.now() / 1000}`);
+  } catch (err) {
+    setStatus(id, `Profile failed with error: '${err.message}'`);
+  }
+}
+
+function setStatus(id, status) {
+  profileStatuses[id] = status;
+}
+
+function getStatus(req, res) {
+  if (!req.params || !req.params.id) {
+    return res.status(403).send('Please indicate profile id');
+  }
+
+  if (!profileStatuses[req.params.id]) {
+    return res.status(404);
+  }
+
+  return res.status(200).send(profileStatuses[req.params.id]);
 }
